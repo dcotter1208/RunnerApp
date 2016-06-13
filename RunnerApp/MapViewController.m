d26665c49208075d1792b348a4a625867d520853
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-13 16:28:30 -0400

    app zooms to users location on the map when the view loads

--- RunnerApp/MapViewController.m
+++ RunnerApp/MapViewController.m
@@ -15,13 +15,16 @@
 
 @end
 
+CLLocation *newLocation, *oldLocation;
+MKCoordinateRegion userLocation;
+
 @implementation MapViewController 
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    [self mapSetup];
     // Do any additional setup after loading the view.
 }
 
@@ -38,7 +41,7 @@
     self.locations = [NSMutableArray array];
     self.timer = [NSTimer scheduledTimerWithTimeInterval:(1.0) target:self
                                                 selector:@selector(eachSecond) userInfo:nil repeats:YES];
-    [self startLocationUpdates];
+//    [self startLocationUpdates];
     
 }
 
@@ -48,22 +51,58 @@
     
 }
 
-- (void)startLocationUpdates
-{
-    // Create the location manager if this object does not
-    // already have one.
-    if (self.locationManager == nil) {
-        self.locationManager = [[CLLocationManager alloc] init];
+-(void)mapSetup {
+    [_mapView setDelegate:self];
+    [_mapView setShowsUserLocation:true];
+    [self getUserLocation];
+}
+
+-(void)getUserLocation {
+    
+    if (_locationManager == nil) {
+        _locationManager = [[CLLocationManager alloc]init];
+        [_locationManager setDelegate:self];
+        [_locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+        [_locationManager setActivityType:CLActivityTypeFitness];
+        [_locationManager requestWhenInUseAuthorization];
+        [_locationManager setDistanceFilter:10];
+        [_locationManager startUpdatingLocation];
+        newLocation = _locationManager.location;
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    newLocation = [locations lastObject];
+    
+    if (locations.count > 1) {
+        NSUInteger newLocationIndex = [locations indexOfObject:newLocation];
+        oldLocation = [locations objectAtIndex:newLocationIndex];
+    } else {
+        oldLocation = nil;
     }
     
-    self.locationManager.delegate = self;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    self.locationManager.activityType = CLActivityTypeFitness;
+    userLocation = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 300.0, 300.0);
+    [_mapView setRegion:userLocation animated:YES];
     
-    // Movement threshold for new events.
-    self.locationManager.distanceFilter = 10; // meters
-    
-    [self.locationManager startUpdatingLocation];
 }
+
+//- (void)startLocationUpdates
+//{
+//    // Create the location manager if this object does not
+//    // already have one.
+//    if (self.locationManager == nil) {
+//        self.locationManager = [[CLLocationManager alloc] init];
+//    }
+//    
+//    self.locationManager.delegate = self;
+//    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//    self.locationManager.activityType = CLActivityTypeFitness;
+//    
+//    // Movement threshold for new events.
+//    self.locationManager.distanceFilter = 10; // meters
+//    
+//    [self.locationManager startUpdatingLocation];
+//}
 
 @end
