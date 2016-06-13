d6bf6e2fb2575e34ec169274522fd5a8778008fb
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-13 14:48:55 -0400

    created initial storyboard and added mapView with start and stop buttons

---
+++ RunnerApp/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  RunnerApp
+//
+//  Created by DetroitLabs on 6/13/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *startAndPauseButton;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startAndPauseButtonPressed:(id)sender {
+    
+}
+
+
+- (IBAction)stopButtonPressed:(id)sender {
+    
+    
+}
+
+@end
