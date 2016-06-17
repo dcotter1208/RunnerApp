b6aef3e3ae8009d138363706b3c0e9a1527147aa
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-17 13:43:36 -0400

    SignUpViewController added that is seperate from LoginViewController.

---
+++ RunnerApp/SignUpViewController.m
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.m
+//  RunnerApp
+//
+//  Created by DetroitLabs on 6/17/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+@property (weak, nonatomic) IBOutlet UITextField *repeatPasswordTF;
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)signUpPressed:(id)sender {
+    
+    
+}
+
+
+@end
