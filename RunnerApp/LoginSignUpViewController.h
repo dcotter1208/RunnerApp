de130dc04b8366b915795a323da36bb1a6b406d0
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-17 10:28:17 -0400

    added login and signup View Controller

---
+++ RunnerApp/LoginSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginSignUpViewController.h
+//  RunnerApp
+//
+//  Created by DetroitLabs on 6/17/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginSignUpViewController : UIViewController
+
+@end
---
+++ RunnerApp/LoginSignUpViewController.m
@@ -0,0 +1,33 @@
+//
+//  LoginSignUpViewController.m
+//  RunnerApp
+//
+//  Created by DetroitLabs on 6/17/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "LoginSignUpViewController.h"
+
+@interface LoginSignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+
+@end
+
+@implementation LoginSignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)loginButtonPressed:(id)sender {
+    
+}
+
+
+@end
